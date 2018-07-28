62cf43e7cc0ebc86b6a582c63630034d78ded435
mmarashan <maxim@ubuntu.ubuntu-domain>
2018-07-28 22:10:44 +0300

    create core project

---
+++ message_port_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MessagePortAppConfig(AppConfig):
+    name = 'message_port_app'
---
+++ message_port_app/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Message(models.Model):
+    sender_address = models.TextField()
+    reciever_address = models.TextField()
+    text = models.TextField()
+    date_created = models.DateTimeField(default=timezone.now)
+
+    FROM_YASHIK_MESSAGE = 'FROM_YASHIK_MESSAGE'
+    FROM_USER_MESSAGE = 'FROM_USER_MESSAGE'
+
+    MESSAGE_TYPES = (
+        (FROM_YASHIK_MESSAGE, 'Сообщение от ящика'),
+        (FROM_USER_MESSAGE, 'Сообщение от пользователя')
+    )
+
+    type = models.CharField(max_length=64, choices=MESSAGE_TYPES, null=True, blank=True)
+
+
+    def __str__(self):
+        return '{}-{}-{}'.format(self.sender_address, self.type, self.text)
